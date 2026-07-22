a30632774c4a3cb74f25aea0526ba669005918ee
davv24 <emilio.gavilanez@epn.edu.ec>
2026-07-21 19:00:50 -0500

    carpeta duplicada

--- Proyecto-de-poo-main/proyectoAgendamientoCitas/src/main/java/com/esfot/epn/proyectotalleres/proyectoagendamientocitas/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.esfot.epn.proyectotalleres.proyectoagendamientocitas;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
